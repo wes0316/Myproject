778ed17df8a40266ddb6e4fb68d922f1e88c1623
unknown <wes0316@gmail.com>
2022-01-28 14:09:10 +0800

    Initial commit

---
+++ src/main/java/com/itri/Hello.java
@@ -0,0 +1,12 @@
+package com.itri;
+
+public class Hello {
+
+    public static void main(String[] args) {
+       // System.out.println("Hello");
+
+        Person p = new Person();
+        p.hello();
+
+    }
+}
---
+++ src/main/kotlin/com/itri/Hello.kt
@@ -0,0 +1,19 @@
+package com.itri
+
+    fun main(args:Array<String>)
+    {
+       //println("Hello Kt")
+       // Human().hello();
+        val h = Human();
+        h.hello()
+
+    }
+
+class  Human {
+
+    fun hello(){
+
+        println("Hello Human")
+
+    }
+}
